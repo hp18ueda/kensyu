1b209821982f6984e799e4aecbe76f626ea8b67a
minobe yuuya <embex.kensyu00121601003@gmail.com>
2018-04-27 17:12:38 +0900

    ok

---
+++ 新しいフォルダー/mtg.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <mtg.h>
+int Tolarian Academy
+{
+	printf("カード名: トレイリアのアカデミー\nカードタイプ:伝説の土地\nテキスト: (T) あなたのコントロールするアーティファクト一つにつき、(青)を加える")
+	if
+	ManaPool+=Artifact
+	
+}
+
+
+int Artifacts=0;
+int Lands=0;
+int Enchants=0;
+int Cretures=0;
+int hand=0;
+int Librarys=60;
+int ManaPool=0;
+int Marigun=7;
+int Hand=0;
+
+
+int main(void){
+	int i,x,y,z;
+	for(i=0;i<Marigun;i++){
+		
+	}
+	return(0);
+}
---
+++ 新しいフォルダー/mtg.h
@@ -0,0 +1,8 @@
+#ifndef mtg_h
+#define mtg_h
+
+extern int Tolarian Academy( int mana);
+extern int funcMUL( int param);
+extern int funcADD( int param);
+
+#endif		/* #ifndef OP_H */
